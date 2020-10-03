831d440452f1a98c677bbf1acf4098673680ca4b
indiamela <taishi.kusunose@gmail.com>
2020-10-03 13:51:21 +0900

    Initial Commit

---
+++ CardApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CardApp
+//
+//  Created by 楠瀬大志 on 2020/10/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
